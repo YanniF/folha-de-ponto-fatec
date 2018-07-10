d1398b29c55afa4a44865e08c74da09fd468f791
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-07-10 17:28:38 -0300

    Páginas de cadastro e impressão (professores)

--- js/main.js
+++ js/main.js
@@ -1,49 +1,65 @@
-const modal = document.getElementById('modal-wrapper');
-const pesquisarInput = document.getElementById('pesquisar-input');
-const pesquisarIcon = document.getElementById('pesquisar-icon');
+let pagina = (window.location.pathname); // pega o endereço
+pagina = pagina.substring(pagina.lastIndexOf('/') + 1); // pega o nome da página
 
-//---  PESQUISAR  ---//
+// carrega os eventos dependendo do que a página usa
+if(pagina.includes('cadastro') || pagina.includes('feriado')) {
+  carregarModal();
+  carregarPesquisar();
+}
+else if(pagina.includes('impressao')) {
+  carregarPesquisar();
+}
 
-// clica no span que envolve o ícone para focar no input, aumentando o tamanho
-pesquisarIcon.addEventListener('click', function() {
-  pesquisarInput.focus();
-});
+function carregarPesquisar() {
 
-// clica fora do input, limpa o campo
-pesquisarInput.addEventListener('blur', function() {
-  pesquisarInput.value = "";
-});
+  const pesquisarInput = document.getElementById('pesquisar-input');
+  const pesquisarIcon = document.getElementById('pesquisar-icon');
 
-pesquisarInput.addEventListener('keyup', function(e) {
-  const texto = e.target.value.toLowerCase();
+  // clica no span que envolve o ícone para focar no input, aumentando o tamanho
+  pesquisarIcon.addEventListener('click', function() {
+    pesquisarInput.focus();
+  });
 
-  document.querySelectorAll('.tabela-item').forEach((linha) => {
-    const item = linha.cells[0].textContent;
+  // clica fora do input, limpa o campo
+  pesquisarInput.addEventListener('blur', function() {
+    pesquisarInput.value = "";
+  });
 
-    if(item.toLowerCase().indexOf(texto) != -1) {
-      linha.style.display = 'block';
-    }
-    else {
-      linha.style.display = 'none';
-    }
-  })
-});
+  // a cada tecla pressionada, vai verificar se existe aquele conjunto de caracteres na 1ª célula da linha
+  pesquisarInput.addEventListener('keyup', function(e) {
+    const texto = e.target.value.toLowerCase();
 
+    document.querySelectorAll('.tabela-item').forEach((linha) => {
+      const item = linha.cells[0].textContent;
 
-//--- MODAL ---//
-document.getElementById('btnMostrarModal').addEventListener('click', function() {
-  modal.classList.add('flex');
-  modal.classList.remove('hidden');
-});
+      if(item.toLowerCase().indexOf(texto) != -1) {
+        linha.style.display = 'table-row';
+      }
+      else {
+        linha.style.display = 'none';
+      }
+    })
+  });
+}
 
-document.querySelector('.btn-fechar').addEventListener('click', function() {
-  modal.classList.add('hidden');
-  modal.classList.remove('flex');
-});
+function carregarModal() {
+  
+  const modal = document.getElementById('modal-wrapper');
 
-modal.addEventListener('click', function(e) {
-  if(e.target === modal) {
+  document.getElementById('btnMostrarModal').addEventListener('click', function() {
+    modal.classList.add('flex');
+    modal.classList.remove('hidden');
+  });
+  
+  document.querySelector('.btn-fechar').addEventListener('click', function() {
     modal.classList.add('hidden');
     modal.classList.remove('flex');
-  }    
-});
+  });
+  
+  modal.addEventListener('click', function(e) {
+    if(e.target === modal) {
+      modal.classList.add('hidden');
+      modal.classList.remove('flex');
+    }    
+  });
+}
