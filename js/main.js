137e7d84f154b00189e07bf449b9a49ae6a3edb9
Yanni Fraga <yanni_fernanda@hotmail.com>
2018-07-11 17:25:52 -0300

    Funcionalidade para criar e apagar linhas no horário dos professores

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 let pagina = (window.location.pathname); // pega o endereço
-pagina = pagina.substring(pagina.lastIndexOf('/') + 1); // pega o nome da página
+// pagina = pagina.substring(pagina.lastIndexOf('/') + 1); // pega o nome da página
 
 // carrega os eventos dependendo do que a página usa
-if(pagina.includes('cadastro') || pagina.includes('feriado')) {
+if(pagina.includes('cadastro')|| pagina.includes('feriado')) {
   carregarModal();
   carregarPesquisar();
 }
-else if(pagina.includes('impressao')) {
+
+if(pagina.includes('impressao')) {
   carregarPesquisar();
 }
+
+if(pagina.includes('prof/cadastro')) {
+  alterarTabela();
+}
+
+
+//--- FUNCTIONS ---//
 
 function carregarPesquisar() {
 
@@ -61,5 +69,55 @@ function carregarModal() {
       modal.classList.add('hidden');
       modal.classList.remove('flex');
     }    
+  });
+}
+
+function alterarTabela() {
+  const btnAddLinha = document.getElementById('btnAddLinha');
+  const tbody = document.getElementById('horario');
+  const btnApagarLinha = document.querySelector('.delete');
+  
+  btnAddLinha.addEventListener('click', function(e) {
+
+    const linha = document.createElement('tr');
+
+    linha.innerHTML = `
+      <td>
+        <select name="dia" id="dia" class="modal-select">
+          <option value="2">Seg.</option>
+          <option value="3">Ter.</option>
+          <option value="4">Qua.</option>
+          <option value="5">Qui.</option>
+          <option value="6">Sex.</option>
+          <option value="7">Sab.</option>
+        </select>
+      </td>
+      <td>
+        <input type="time" class="modal-input" name="entrada" placeholder="00:00">
+      </td>
+      <td>
+        <input type="time" class="modal-input" name="saida" placeholder="00:00">
+      </td>
+      <td>
+        <a href="#" class="btn btn-apagar delete" title="Apagar horário"><i class="fas fa-trash-alt"></i></a>
+      </td>
+    `;
+
+    tbody.appendChild(linha);
+
+    e.preventDefault();
+  });
+
+  tbody.addEventListener('click', function(target) {
+    
+    const alvo = target.target;
+    if(alvo.className === 'btn btn-apagar delete') {
+      alvo.parentElement.parentElement.remove();
+    }
+    // melhorar isso aqui
+    else if(alvo.parentElement.className.baseVal  == 'svg-inline--fa fa-trash-alt fa-w-14') {
+      alvo.parentElement.parentElement.parentElement.parentElement.remove(); // dai me forças
+    }
+    
   });
 }
